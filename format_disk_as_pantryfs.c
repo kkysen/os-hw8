5aa763915144abcca203d8849e374cb8f25a9715
Khyber Sen <kkysen@gmail.com>
2021-12-11 21:58:36 -0500

    Simplified the error handling to what the original did.

--- format_disk_as_pantryfs.c
+++ format_disk_as_pantryfs.c
@@ -18,7 +18,7 @@
 
 struct dentry_args {
 	const char *filename; // cstring
-	uint8_t inode_offset;
+	uint8_t index;
 };
 
 struct directory_file_args {
@@ -38,31 +38,19 @@ struct file_args {
 	};
 };
 
-bool p(bool ok, const char *message)
+void p(bool condition, const char *message)
 {
-	printf("[%s] %s\n", ok ? " OK " : "FAIL", message);
-	return ok;
+	printf("[%s] %s\n", condition ? " OK " : "FAIL", message);
+	if (!condition)
+		exit(1);
 }
 
-int format_disk(const char *disk_image_path, const struct file_args *files)
+void format_disk(const char *disk_image_path, const struct file_args *files)
 {
-	int e = 0;
+	int fd;
+	const struct file_args *file;
+	size_t i;
 	ssize_t n_written;
-
-	const int fd = open(disk_image_path, O_RDWR);
-	if (fd == -1) {
-		e = -1;
-		perror("Error opening the device");
-		goto ret;
-	}
-
-	struct stat stats = { 0 };
-
-	if (fstat(fd, &stats) == -1) {
-		e = -1;
-		goto ret;
-	}
-
 	struct pantryfs_super_block super_block = {
 		.version = 1,
 		.magic = PANTRYFS_MAGIC_NUMBER,
@@ -70,19 +58,13 @@ int format_disk(const char *disk_image_path, const struct file_args *files)
 		.free_data_blocks = { 0 },
 		.__padding__ = { 0 },
 	};
-
 	char inode_buf[PFS_BLOCK_SIZE] = { 0 };
 	struct pantryfs_inode *inodes = (struct pantryfs_inode *)inode_buf;
 
-	if (!p(lseek(fd, sizeof(super_block) + sizeof(inode_buf), SEEK_SET) !=
-		       -1,
-	       "Seek past super and inode blocks (writing them at the end)")) {
-		e = -1;
-		goto restore_file;
-	}
-
-	const struct file_args *file;
-	size_t i;
+	fd = open(disk_image_path, O_RDWR);
+	p(fd != -1, "Error opening the device");
+	p(lseek(fd, sizeof(super_block) + sizeof(inode_buf), SEEK_SET) != -1,
+	  "Seek past super and inode blocks (writing them at the end)");
 
 	for (file = files, i = 0; file->mode; file++, i++) {
 		mode_t perms = file->mode & 0777;
@@ -90,6 +72,7 @@ int format_disk(const char *disk_image_path, const struct file_args *files)
 		unsigned int nlink;
 		uint64_t file_size;
 		char data_buf[PFS_BLOCK_SIZE] = { 0 };
+		struct timespec current_time = { 0 };
 
 		switch (type) {
 		case __S_IFDIR: {
@@ -106,12 +89,11 @@ int format_disk(const char *disk_image_path, const struct file_args *files)
 			     dentry_arg->filename; dentry_arg++, j++) {
 				dentry = &dentries[j];
 				dentry->inode_no = PANTRYFS_ROOT_INODE_NUMBER +
-						   dentry_arg->inode_offset;
+						   dentry_arg->index;
 				dentry->active = 1;
 				strncpy(dentry->filename, dentry_arg->filename,
 					sizeof(dentry->filename));
-				if (S_ISDIR(files[dentry_arg->inode_offset]
-						    .mode)) {
+				if (S_ISDIR(files[dentry_arg->index].mode)) {
 					// add 1 for each subdirectory '..' link
 					nlink++;
 				}
@@ -128,19 +110,15 @@ int format_disk(const char *disk_image_path, const struct file_args *files)
 			break;
 		}
 		default: {
-			fprintf(stderr,
-				"unknown file type: not directory or regular file: %o, i = %zu\n",
-				file->mode & S_IFMT, i);
-			e = -1;
-			goto restore_file;
+			p(false, "file type not directory or regular file");
+			break;
 		}
 		}
-
-		struct timespec current_time = { 0 };
-
 		clock_gettime(CLOCK_REALTIME, &current_time);
 
-		struct pantryfs_inode inode = {
+		SETBIT(super_block.free_inodes, i);
+		SETBIT(super_block.free_data_blocks, i);
+		inodes[i] = (struct pantryfs_inode) {
 			.mode = file->mode | perms,
 			.uid = getuid(),
 			.gid = getgid(),
@@ -151,48 +129,20 @@ int format_disk(const char *disk_image_path, const struct file_args *files)
 			.data_block_number = PANTRYFS_ROOT_DATABLOCK_NUMBER + i,
 			.file_size = file_size,
 		};
-
-		SETBIT(super_block.free_inodes, i);
-		SETBIT(super_block.free_data_blocks, i);
-		inodes[i] = inode;
-
 		n_written = write(fd, data_buf, sizeof(data_buf));
-		if (!p(n_written == sizeof(data_buf), "Write a data block")) {
-			e = -1;
-			goto restore_file;
-		}
+		p(n_written == sizeof(data_buf), "Write a data block");
 	}
 
-	if (!p(lseek(fd, 0, SEEK_SET) != -1,
-	       "Seek back to super and inode blocks")) {
-		e = -1;
-		goto restore_file;
-	}
+	p(lseek(fd, 0, SEEK_SET) != -1, "Seek back to super and inode blocks");
 	n_written = write(fd, (void *)&super_block, sizeof(super_block));
-	if (!p(n_written == PFS_BLOCK_SIZE, "Write superblock")) {
-		e = -1;
-		goto ret;
-	}
+	p(n_written == PFS_BLOCK_SIZE, "Write superblock");
 	n_written = write(fd, (void *)&inode_buf, sizeof(inode_buf));
-	if (!p(n_written == sizeof(inode_buf), "Write inode block")) {
-		e = -1;
-		goto ret;
-	}
+	p(n_written == sizeof(inode_buf), "Write inode block");
 
-	if (!(p(fsync(fd) == 0, "Flush writes to disk"))) {
-		e = -1;
-		goto ret;
-	}
+	p(fsync(fd) == 0, "Flush writes to disk");
 
 	printf("Device [%s] formatted successfully.\n", disk_image_path);
-	goto close;
-
-restore_file:
-	ftruncate(fd, stats.st_size);
-close:
 	close(fd);
-ret:
-	return e;
 }
 
 int main(int argc, char *argv[])
@@ -204,47 +154,27 @@ int main(int argc, char *argv[])
 	const struct file_args files[] = {
 		{
 			.mode = S_IFDIR,
-			.dir = {
-				.dentries = {
-					{
-						.filename = "hello.txt",
-						.inode_offset = 1,
-					},
-					{
-						.filename = "members",
-						.inode_offset = 2,
-					},
-				},
-			},
+			.dir = { .dentries = {
+				{.filename = "hello.txt", .index = 1},
+				{.filename = "members", .index = 2},
+			}},
 		},
 		{
 			.mode = S_IFREG,
-			.file = {
-				.data = "Hello world\n",
-			},
+			.file = { .data = "Hello world\n" },
 		},
 		{
 			.mode = S_IFDIR,
-			.dir = {
-				.dentries = {
-					{
-						.filename = "names.txt",
-						.inode_offset = 3,
-					},
-				},
-			},
+			.dir = { .dentries = {
+				{.filename = "names.txt", .index = 3},
+			}},
 		},
 		{
 			.mode = S_IFREG,
-			.file = {
-				.data = "Isabelle, Khyber, WenChing\n",
-			},
+			.file = { .data = "Isabelle, Khyber, WenChing\n" },
 		},
 		{0},
 	};
-	if (format_disk(argv[1], files) == -1) {
-		perror("");
-		return EXIT_FAILURE;
-	}
+	format_disk(argv[1], files);
 	return EXIT_SUCCESS;
 }
